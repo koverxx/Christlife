187c8aefce01ff7b475a9614f523b98db45dd72e
Helen Kover <koverhelen@gmail.com>
2024-06-29 17:13:44 +0000

    Updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      fontFamily: {
-        urbanist: [ 'Urbanist', 'sans-serif']
-      }
+      backgroundImage: (theme) => ({
+        "custom-image": "url('/Images/background.png')",
+        fontFamily: {
+          urbanist: ["Urbanist", "sans-serif"],
+        },
+      }),
     },
   },
   plugins: [],
-}
+};
